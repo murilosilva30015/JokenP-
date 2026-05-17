60abe773379a465fe6fb9792aa6a16e529777cdd
murilosilva30015 <murilosilva30015@gmail.com>
2026-05-17 20:31:26 -0300

    sorteador

--- script.js
+++ script.js
@@ -1,78 +1,9 @@
-const convertButton = document.querySelector(".convert-button");
-const currencySelect = document.querySelector(".currency-select");
+function generateNumber() {
+    const min = Math.ceil(document.querySelector('.input-minimo').value)
+    const max = Math.floor(document.querySelector('.input-maximo').value)
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-
-function convertValues() {
-  const inputCurrencyValue = document.querySelector(".input-currency").value;
-const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
-  const currencyValueConverted = document.querySelector(".currency-value");
-
-  const dolarToday = 5.1;
-  const euroToday = 5.9;
-  const libraToday = 6.7;
-  const bitcoinToday = 401.7;
-
-
- currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(inputCurrencyValue);
-
-
-  if (currencySelect.value == "dolar") {
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(inputCurrencyValue / dolarToday);
-  }
-
-  if (currencySelect.value == "euro") {
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-      style: "currency",
-      currency: "EUR",
-    }).format(inputCurrencyValue / euroToday);
-  }
-if (currencySelect.value == "libra") {
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
-      style: "currency",
-      currency: "GBP",
-    }).format(inputCurrencyValue / libraToday);
-  }
-  
-    if (currencySelect.value == "bitcoin") {
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "BTC",
-    }).format(inputCurrencyValue / bitcoinToday);
-  }
-
+    const resultEl = document.getElementById('result');
+    if (resultEl) resultEl.textContent = `O número sorteado foi: ${result}`;
 }
-
-function changeCurrency() {
-  const currencyName = document.getElementById("currency-name");
-  const currencyImg = document.querySelector(".currency-img");
-
-  if (currencySelect.value == "dolar") {
-    currencyName.innerHTML = "Dólar americano";
-    currencyImg.src = "./assets/estados-unidos (1) 1.png";
-  }
-  if (currencySelect.value == "euro") {
-    currencyName.innerHTML = "Euro";
-    currencyImg.src = "./assets/Euro.png";
-  }
-  if (currencySelect.value == "libra") {
-    currencyName.innerHTML = "Libra";
-    currencyImg.src = "./assets/libra 1.png";
-  }
-  if (currencySelect.value == "bitcoin") {
-    currencyName.innerHTML = "Bitcoin";
-    currencyImg.src = "./assets/bitcoin 1.png";
-  }
- 
-  convertValues();
-  }
-
-currencySelect.addEventListener("change", changeCurrency);
-convertButton.addEventListener("click", convertValues);
-
 
